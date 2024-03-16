fcc02581ae444809bdbac3002d32113dc23b685a
00012429 <saidlanibesti@gmail.com>
2024-03-17 01:00:08 +0500

    Models and Database context

---
+++ WAD.CW1.00012429/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WAD.CW1._00012429.Models;
+
+namespace WAD.CW1._00012429.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public DbSet<Product> Products { get; set; }
+		public DbSet<Feedback> Feedbacks { get; set; }
+
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Product>()
+				.HasMany(p => p.Feedbacks)
+				.WithOne(f => f.Product)
+				.HasForeignKey(f => f.ProductId);
+		}
+	}
+}
---
+++ WAD.CW1.00012429/Models/Feedback.cs
@@ -0,0 +1,11 @@
+﻿namespace WAD.CW1._00012429.Models
+{
+	public class Feedback
+	{
+		public int FeedbackId { get; set; }
+		public string Content { get; set; }
+		public DateTime DateCreated { get; set; }
+		public int ProductId { get; set; }
+		public Product Product { get; set; }
+	}
+}
