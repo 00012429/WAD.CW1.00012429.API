d73691efbc11a211ba506215795bd1eb99860dd4
00012429 <saidlanibesti@gmail.com>
2024-03-17 01:00:54 +0500

    Interfaces

---
+++ WAD.CW1.00012429/Interfaces/IFeedbackRepository.cs
@@ -0,0 +1,13 @@
+﻿using WAD.CW1._00012429.Models;
+
+namespace WAD.CW1._00012429.Interfaces
+{
+	public interface IFeedbackRepository
+	{
+		IEnumerable<Feedback> GetAllByProductId(int productId);
+		Feedback GetById(int id);
+		void Add(Feedback feedback);
+		void Update(Feedback feedback);
+		void Delete(Feedback feedback);
+	}
+}
---
+++ WAD.CW1.00012429/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using WAD.CW1._00012429.Models;
+
+namespace WAD.CW1._00012429.Interfaces
+{
+	public interface IProductRepository
+	{
+		IEnumerable<Product> GetAll();
+		Product GetById(int id);
+		void Add(Product product);
+		void Update(Product product);
+		void Delete(Product product);
+	}
+}
