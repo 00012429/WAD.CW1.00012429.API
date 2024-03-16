692912e405e210fb7093e2a901942d1c1b33268e
00012429 <saidlanibesti@gmail.com>
2024-03-17 01:01:11 +0500

    Repositories with CRUD operations

---
+++ WAD.CW1.00012429/Repositories/FeedbackRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WAD.CW1._00012429.Data;
+using WAD.CW1._00012429.Interfaces;
+using WAD.CW1._00012429.Models;
+
+namespace WAD.CW1._00012429.Repositories
+{
+	public class FeedbackRepository : IFeedbackRepository
+	{
+		private readonly ApplicationDbContext _context;
+
+		public FeedbackRepository(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public IEnumerable<Feedback> GetAllByProductId(int productId)
+		{
+			return _context.Feedbacks
+						   .Where(f => f.ProductId == productId)
+						   .Include(f => f.Product)
+						   .ToList();
+		}
+
+		public Feedback GetById(int id)
+		{
+			var result = _context.Feedbacks
+						   .Include(f => f.Product)
+						   .FirstOrDefault(f => f.FeedbackId == id);
+
+			if (result == null)
+			{
+				return new Feedback();
+			}
+
+			return result;
+		}
+
+		public void Add(Feedback feedback)
+		{
+			_context.Feedbacks.Add(feedback);
+			_context.SaveChanges();
+		}
+
+		public void Update(Feedback feedback)
+		{
+			_context.Feedbacks.Update(feedback);
+			_context.SaveChanges();
+		}
+
+		public void Delete(Feedback feedback)
+		{
+			_context.Feedbacks.Remove(feedback);
+			_context.SaveChanges();
+		}
+	}
+}
---
+++ WAD.CW1.00012429/Repositories/ProductRepository .cs
@@ -0,0 +1,44 @@
+﻿using WAD.CW1._00012429.Data;
+using WAD.CW1._00012429.Interfaces;
+using WAD.CW1._00012429.Models;
+
+namespace WAD.CW1._00012429.Repositories
+{
+	public class ProductRepository : IProductRepository
+	{
+		private readonly ApplicationDbContext _context;
+
+		public ProductRepository(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public IEnumerable<Product> GetAll()
+		{
+			return _context.Products.ToList();
+		}
+
+		public Product GetById(int id)
+		{
+			return _context.Products.Find(id);
+		}
+
+		public void Add(Product product)
+		{
+			_context.Products.Add(product);
+			_context.SaveChanges();
+		}
+
+		public void Update(Product product)
+		{
+			_context.Products.Update(product);
+			_context.SaveChanges();
+		}
+
+		public void Delete(Product product)
+		{
+			_context.Products.Remove(product);
+			_context.SaveChanges();
+		}
+	}
+}
